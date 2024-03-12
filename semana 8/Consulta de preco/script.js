5b9baf5ebdc319e0de2160b037849b5306e17a55
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-03-12 19:49:13 -0300

    javascript

---
+++ semana 8/Consulta de preco/script.js
@@ -0,0 +1,6 @@
+console.log('oi :D')
+//função para consulta
+function consulta() {
+    console.log("function -> consulta")
+    document.getElementById("consultaResp").innerHTML = "O valor do produto é: "
+}
