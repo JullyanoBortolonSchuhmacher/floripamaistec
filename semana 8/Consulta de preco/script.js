783c47239db7bae2890a40d5c58336ca9c7f5d51
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-03-12 22:56:03 -0300

    atualizando o javascript para visualizar no html

--- semana 8/Consulta de preco/script.js
+++ semana 8/Consulta de preco/script.js
@@ -1,6 +1,23 @@
 console.log('oi :D')
 //função para consulta
+
 function consulta() {
-    console.log("function -> consulta")
-    document.getElementById("consultaResp").innerHTML = "O valor do produto é: "
+	console.log("function -> consulta")
+	var resposta_consulta = document.getElementById('consultaResp').innerHTML = "O valor do produto é: "
+    console.log(resposta_consulta)
 }
+
+function comprar() {
+    console.log("function -> comprar")  
+    var resposta_comprar =  document.getElementById('consultaResp').innerHTML = "carrinho:"
+    console.log(resposta_comprar)
+    //exibindo o input ao html//
+    let text = document.getElementById("nomeCodigoProduto").value.trim().toLowerCase();
+    let list = document.getElementById("carrinho").innerHTML;
+    if (text == "" || text == null){ 
+        alert("Digite o código ou nome do produto!");
+    } else {
+    list += "<li>" +text+ "</li>"
+    document.getElementById("carrinho").innerHTML=list 
+    }
+}  
