21fc83c3fdab7e43a3436f0d6f43900c5d6f7587
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-03-15 16:46:45 -0300

    .

--- semana 8/Consulta de preco/script.js
+++ semana 8/Consulta de preco/script.js
@@ -3,13 +3,16 @@ console.log('oi :D')
 
 function consulta() {
 	console.log("function -> consulta")
-	var resposta_consulta = document.getElementById('consultaResp').innerHTML = "O valor do produto é: "
+	var resposta_consulta = document.getElementById('consultaResp').innerHTML = "O valor do produto é "; 
     console.log(resposta_consulta)
 }
 
+function limparInput(){
+    document.getElementById("nomeCodigoProduto").value='';
+};
+
 function comprar() {
     console.log("function -> comprar")  
-    var resposta_comprar =  document.getElementById('consultaResp').innerHTML = "carrinho:"
     console.log(resposta_comprar)
     //exibindo o input ao html//
     let text = document.getElementById("nomeCodigoProduto").value.trim().toLowerCase();
@@ -19,5 +22,34 @@ function comprar() {
     } else {
     list += "<li>" +text+ "</li>"
     document.getElementById("carrinho").innerHTML=list 
+    var resposta_comprar =  document.getElementById('h2carrinho').innerHTML = "carrinho:"    
+    //limpando input
+    limparInput()        
     }
 }  
+
+function vlrTotal() {
+    console.log("function -> vlrTotal")
+    var carrinho = document.getElementsByTagName("LI");
+    var total = 0;
+    for (i = 0 ; i <carrinho.length; i++) {
+        total += Number(carrinho[i].innerText);
+    };
+    return document.getElementById("vlrTotal").innerHTML = "Valor Total: ";
+}
+
+/*     let itens = list.split(',');
+
+    //calculando
+    let total = itens.reduce((acc, item) => {
+        let valor = item.trim().toLowerCase();
+        return acc + parseFloat(valor);
+    },0);
+        //console
+        console.log(total)
+        console.log(itens)
+} */
+
+
+//local storage
+
