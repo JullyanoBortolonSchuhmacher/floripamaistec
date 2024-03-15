54d6f4729539a9d857840cfeff0c759ca48c8d70
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-03-15 17:58:26 -0300

    .,

--- semana 8/Consulta de preco/script.js
+++ semana 8/Consulta de preco/script.js
@@ -1,55 +1,65 @@
 console.log('oi :D')
 //função para consulta
 
-function consulta() {
-	console.log("function -> consulta")
-	var resposta_consulta = document.getElementById('consultaResp').innerHTML = "O valor do produto é "; 
-    console.log(resposta_consulta)
+let carrinho = [];
+
+function limparInput() {
+    document.getElementById("nomeCodigoProduto").value = '';
+    document.getElementById("valorProduto").value = '0';
 }
 
-function limparInput(){
-    document.getElementById("nomeCodigoProduto").value='';
-};
+function consulta() {
+    console.log("function -> consulta");
+    // Display the "Valor Total" with an empty value as a placeholder
+    document.getElementById('showInHtml').innerHTML = "O valor do produto é ";
+    limparInput();
+}
 
 function comprar() {
-    console.log("function -> comprar")  
-    console.log(resposta_comprar)
-    //exibindo o input ao html//
-    let text = document.getElementById("nomeCodigoProduto").value.trim().toLowerCase();
-    let list = document.getElementById("carrinho").innerHTML;
-    if (text == "" || text == null){ 
-        alert("Digite o código ou nome do produto!");
-    } else {
-    list += "<li>" +text+ "</li>"
-    document.getElementById("carrinho").innerHTML=list 
-    var resposta_comprar =  document.getElementById('h2carrinho').innerHTML = "carrinho:"    
-    //limpando input
-    limparInput()        
-    }
-}  
+    console.log("function -> comprar");
 
-function vlrTotal() {
-    console.log("function -> vlrTotal")
-    var carrinho = document.getElementsByTagName("LI");
-    var total = 0;
-    for (i = 0 ; i <carrinho.length; i++) {
-        total += Number(carrinho[i].innerText);
-    };
-    return document.getElementById("vlrTotal").innerHTML = "Valor Total: ";
+    // nome e valor dos produtos
+    let nomeProduto = document.getElementById("nomeCodigoProduto").value.trim().toLowerCase();
+    let valorProduto = document.getElementById('precoProduto').value.trim();
+
+    // valores não são vazios
+    if (nomeProduto !== '' && valorProduto !== '') {
+        // adicionando ao carrinho
+        carrinho.push({ 'nome': nomeProduto, 'preco': valorProduto });
+
+        // mostrando no carrinho
+        let list = document.getElementById("carrinho");
+        list.innerHTML += `<li> ${nomeProduto}, R$ ${valorProduto}</li>`;
+
+    } else {
+        alert("Por favor, inserir valores validos!");
+    }
+
+    // valor total
+    calculaTotal();
 }
 
-/*     let itens = list.split(',');
+function calculaTotal() {
+    console.log("function -> calculaTotal");
+    let total = 0;
+    let carrinhoList = document.getElementById("carrinho");
 
-    //calculando
-    let total = itens.reduce((acc, item) => {
-        let valor = item.trim().toLowerCase();
-        return acc + parseFloat(valor);
-    },0);
-        //console
-        console.log(total)
-        console.log(itens)
-} */
+    // calcula a soma dos produtos
+    for (i = 0; i < carrinho.length; i++) {
+        total += Number(carrinho[i].preco);
+    }
 
+    // mostrar o valor total
+    document.getElementById("showInHtml").innerHTML = "Valor Total: R$ " + total.toFixed(2);
+}
 
-//local storage
+function carrinhoVazio() {
+    console.log('function -> carrinhoVazio')
+    if (carrinho.length == 0) {
+        return document.getElementById("showInHtml").innerHTML = "Carrinho está vazio";
+    } else {
+        return false;
+    }
+} 
 
+carrinhoVazio()
