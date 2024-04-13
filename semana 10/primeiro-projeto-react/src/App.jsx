3eb016e2f958d98024f30ddb870cd47bff1e0d5c
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-04-12 22:57:03 -0300

    semana 12

--- semana 10/primeiro-projeto-react/src/App.jsx
+++ semana 10/primeiro-projeto-react/src/App.jsx
@@ -1,52 +1,17 @@
-import CardTrilha from "./components/CardTrilha";
 import "./App.css";
-import React, { useState, useEffect } from 'react';
-import header from "./components/header";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import {TrilhasContextProvider} from "./context/TrilhasContext"
 
 function App() {
-    const [dadosTrilha, setDadosTrilha] = useState(null);
-    const [header, setHeader] = useState(null);
-    
-
-    useEffect(() => {
-      const fetchDadosTrilha = async() => {
-        const response = await fetch('/dataLagoa.json')
-        const json =  await response.json();
-        setDadosTrilha(json);
-      };
-      fetchDadosTrilha();
-    }, []);
-  
     return (
       <>
-        <div className="pageHeader">
-          <header>
-            {header}
-            {/* <h1 id="hover">
-              <a href="#" className="h1header">
-                Adventure Trails FD
-              </a>
-            </h1>
-            <nav>
-              <a id="hover" href="#explorar">
-                Explorar Trilhas
-              </a>
-              <a id="hover" href="#cadastro">
-                Cadastrar Trilhas
-              </a>
-            </nav> */}
-          </header>
-        </div>
-        <h1 className="titulo">Explore trilhas incríveis</h1>
-        <div className="container">
-        {dadosTrilha ? (
-         <CardTrilha dadosTrilha={dadosTrilha} />
-          ) : (
-        <div>Carregando dados da trilha...</div>
-        )}
-        </div>
+        <TrilhasContextProvider>
+          <Header />
+          <Outlet />
+        </TrilhasContextProvider>
       </>
     );
   }
 
-export default App
+export default App;
