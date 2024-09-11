78b6df7ff8d79813c36e842c6ea4e94348e4db4e
Jullyano Schuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-09-11 12:35:07 -0300

    strutura do projeto de revisão

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <header>
+        <div className="mudaTema">
+          <select id="selcTema">
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+          </select>
+        </div>
+      </header>
+
+      <h1> APP </h1>
+    </>
+  );
+}
+
+export default App;
